1ef883e804ee617de6b660da3d8815422b0bc235
Jyogita Thakur <jyogitathakur@gmail.com>
2022-03-28 16:31:26 +0530

    Added : Day-1

---
+++ Day-1/Day-1.js
@@ -0,0 +1,8 @@
+function randomNumberGeneratorInRange(rangeStart, rangeEnd) {
+	// write your solution here
+const number = Math.floor(Math.random()* (rangeEnd - rangeStart) + rangeStart)
+	return number
+}
+
+console.log(`My random number: ${randomNumberGeneratorInRange(10, 50)}`)
+console.log(`My random number: ${randomNumberGeneratorInRange(100, 200)}`)
---
+++ Day-1/Readme.md
@@ -0,0 +1,1 @@
+## Function which returns a random number in the given range

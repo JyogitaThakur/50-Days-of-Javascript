57347bc806175c55ae35f968ccf0699646f1b0da
JyogitaThakur <77715321+JyogitaThakur@users.noreply.github.com>
2022-04-13 19:45:05 +0530

    Refactor. Updated the Code

--- Day-42/index.js
+++ Day-42/index.js
@@ -9,10 +9,3 @@ function sumTwoSmallestNumbers(numbers) {
 console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]))
 console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43]))
 console.log(sumTwoSmallestNumbers([23, 71, 33, 82, 1]))
-
-
-let A = [[1,2],[3,4]]
-
-let numbers = A.map((num,i)=> {
-    num.map(item => console.log(item))
-})

03177695f582007bf0c622002de485461ff5b314
Jyogita Thakur <jyogitathakur@gmail.com>
2022-04-14 13:16:55 +0530

    Refactor. Updated the code

--- Day-42/index.js
+++ Day-42/index.js
@@ -9,3 +9,4 @@ function sumTwoSmallestNumbers(numbers) {
 console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]))
 console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43]))
 console.log(sumTwoSmallestNumbers([23, 71, 33, 82, 1]))
+
